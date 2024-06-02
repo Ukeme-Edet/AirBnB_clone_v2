0a9c932aae4f39cd5483d9dff3b8ecf0df3d83ff
Ukeme Edet <ukemeedet2207@gmail.com>
2024-06-02 02:50:30 +0100

    Refactor User model to include email, password, first_name, and last_name columns

--- models/user.py
+++ models/user.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+from sqlalchemy import Column, String
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """This class defines a user by various attributes"""
+    """
+    Represents a user in the system.
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    Attributes:
+        email (str): The email address of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
+
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)

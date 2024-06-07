2376110e6203b03d5e2400cb66ed4e7ee9c75be7
Ukeme Edet <ukemeedet2207@gmail.com>
2024-06-07 22:57:12 +0100

    Refactor Amenity model to use explicit table name for place_amenity relationship

--- models/amenity.py
+++ models/amenity.py
@@ -9,25 +9,6 @@ from sqlalchemy import Column, ForeignKey, String, Table
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 
-place_amenity = Table(
-    "place_amenity",
-    Base.metadata,
-    Column(
-        "place_id",
-        String(60),
-        ForeignKey("places.id"),
-        primary_key=True,
-        nullable=False,
-    ),
-    Column(
-        "amenity_id",
-        String(60),
-        ForeignKey("amenities.id"),
-        primary_key=True,
-        nullable=False,
-    ),
-)
-
 
 class Amenity(BaseModel, Base):
     """
@@ -40,4 +21,4 @@ class Amenity(BaseModel, Base):
 
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenity = relationship("Place", secondary=place_amenity)
+    place_amenity = relationship("Place", secondary="place_amenity")

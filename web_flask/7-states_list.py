1eb2cd37fa6363fcf51fde9be16c04c1df36c24e
Ukeme Edet <ukemeedet2207@gmail.com>
2024-06-29 19:54:32 +0100

    Refactor /states_list route to use State model for retrieving all State objects

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,10 +5,11 @@ This script starts a Flask web application.
 The application listens on 0.0.0.0, port 5000.
 
 Routes:
-	- /states_list: Displays an HTML page with a list of all State objects.
+    - /states_list: Displays an HTML page with a list of all State objects.
 """
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -27,7 +28,7 @@ def states_list():
     Returns:
             HTML: The list of State objects.
     """
-    states = storage.all("State")
+    states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
 
 

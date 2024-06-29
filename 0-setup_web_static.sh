6f683a5d3e5abe8eef6cf007b062ff5398e8993d
Ukeme Edet <ukemeedet2207@gmail.com>
2024-06-29 16:13:57 +0100

    chore: Update .gitignore to exclude venv and web_static directories

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,6 +6,28 @@ sudo mkdir -p /data/web_static/{releases/test,shared}
 sudo echo "<h1>Test</h1>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
+sudo echo "server {
+	listen 80;
+	listen [::]:80 default_server;
+
+	root /var/www/html;
+	index index.html index.htm index.nginx-debian.html;
+
+	server_name _;
+
+	location /redirect_me {
+		return 301 https://youtu.be/dQw4w9WgXcQ?si=c5SSrhdJz17xP0kV;
+	}
+
+	error_page 404 /404.html;
+	location = /404.html {
+		root /var/www/html;
+		internal;
+	}
+
+	location /hbnb_static {
+		alias /data/web_static/current;
+	}
+}" | sudo tee /etc/nginx/sites-available/default
 sudo service nginx restart
 exit 0

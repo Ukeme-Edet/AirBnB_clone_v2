0c25f186ea3ce0c02a780929689f1c3da52dd15f
Ukeme Edet <ukemeedet2207@gmail.com>
2024-06-29 16:52:21 +0100

    chore: Refactor Flask routes for improved code readability and maintainability

---
+++ web_flask/3--python_route.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+
+Routes:
+    - /: Displays "Hello HBNB!"
+    - /hbnb: Displays "HBNB"
+    - /c/<text>: Displays "C " followed by the value of the text variable with\
+        underscores replaced by spaces.
+    - /python/<text>: Displays "Python " followed by the value of the text\
+        variable with underscores replaced by spaces and default value\
+        set to "is cool".
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_hbnb():
+    """
+    Displays "Hello HBNB!"
+
+    Returns:
+            str: "Hello HBNB!"
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """
+    Displays "HBNB"
+
+    Returns:
+            str: "HBNB"
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c(text):
+    """
+    Displays "C " followed by the value of the text variable with\
+        underscores replaced by spaces.
+
+    Args:
+        text (str): The text to display.
+
+    Returns:
+        str: The formatted text.
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def python(text="is cool"):
+    """
+    Displays "Python " followed by the value of the text variable with\
+        underscores replaced by spaces and default value set to "is cool".
+
+    Args:
+        text (str): The text to display.
+
+    Returns:
+        str: The formatted text.
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
